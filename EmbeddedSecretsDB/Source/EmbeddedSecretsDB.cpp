a3783826ce7983671231676982ab8dd8e19f22b7
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-03-28 00:16:59 +0000

    Add files

---
+++ EmbeddedSecretsDB/Source/EmbeddedSecretsDB.cpp
@@ -0,0 +1,7 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/DiplodocusDB/SecretsDB/blob/main/LICENSE.txt
+*/
+
+#include "EmbeddedSecretsDB.h"
---
+++ Include/DiplodocusDB/SecretsDB/EmbeddedSecretsDB/EmbeddedSecretsDB.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/DiplodocusDB/SecretsDB/blob/main/LICENSE.txt
+*/
+
+#ifndef _DIPLODOCUSDB_SECRETSDB_EMBEDDEDSECRETSDB_EMBEDDEDSECRETSDB_H_
+#define _DIPLODOCUSDB_SECRETSDB_EMBEDDEDSECRETSDB_EMBEDDEDSECRETSDB_H_
+
+#endif
