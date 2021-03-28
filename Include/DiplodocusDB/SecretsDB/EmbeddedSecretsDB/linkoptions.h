ca3bd9f6efacf830bbde4ec27b4094ccae81c16d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-03-28 00:52:09 +0000

    Add files

---
+++ Include/DiplodocusDB/SecretsDB/EmbeddedSecretsDB/linkoptions.h
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/DiplodocusDB/SecretsDB/blob/main/LICENSE.txt
+*/
+
+#ifndef _DIPLODOCUSDB_SECRETSDB_EMBEDDEDSECRETSDB_LINKOPTIONS_H_
+#define _DIPLODOCUSDB_SECRETSDB_EMBEDDEDSECRETSDB_LINKOPTIONS_H_
+
+#ifdef _DEBUG
+#pragma comment(lib, "DiplodocusEmbeddedSecretsDB-d.lib")
+#else
+#pragma comment(lib, "DiplodocusEmbeddedSecretsDB.lib")
+#endif
+
+#endif
